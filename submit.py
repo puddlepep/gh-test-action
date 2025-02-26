5fba4a1e5d7f682318b07f61396dfbf5b31f34bb
pepper <pepper@peps.place>
2025-02-25 16:08:03 -0800

    hhH

---
+++ submit.py
@@ -0,0 +1,40 @@
+import os
+
+
+def main():
+
+    # collect required inputs
+    config_path = os.getenv('CONFIG_PATH', None)
+    artifact_path = os.getenv('ARTIFACT_PATH', None)
+    asset_name = os.getenv('NAME', None)
+
+    if config_path is None:
+        print('config-path is required!')
+        raise SystemExit
+
+    if artifact_path is None:
+        print('artifact-path is required!')
+        raise SystemExit
+
+    if asset_name is None:
+        print('name is required!')
+        raise SystemExit
+
+    # collect other inputs
+    manufacturer = os.getenv('MANUFACTURER', '')
+    model = os.getenv('MODEL', '')
+    version = os.getenv('VERSION', '')
+    
+    print('OK!')
+    print(f'Using "{config_path}",')
+    print(f'Submitting "{artifact_path}"')
+    print(f'As "{asset_name}"')
+    print('Optional Inputs:')
+    print(f'Manufacturer: "{manufacturer}"') if manufacturer else ''
+    print(f'Model: "{model}"') if model else ''
+    print(f'Version: "{version}"') if version else ''
+
+
+if __name__ == '__main__':
+    main()
+
